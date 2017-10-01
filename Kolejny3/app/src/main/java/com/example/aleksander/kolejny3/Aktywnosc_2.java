0942b2b60317c3633660d7a9ee6817f718c0fc9a
xAlexii99 <k.aleksander@vp.pl>
2017-10-01 13:51:00 +0200

    zadanie do zadania2

--- Kolejny3/app/src/main/java/com/example/aleksander/kolejny3/Aktywnosc_2.java
+++ Kolejny3/app/src/main/java/com/example/aleksander/kolejny3/Aktywnosc_2.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 public class Aktywnosc_2 extends AppCompatActivity {
 
+    private TextView nie;
     private EditText tak;
     private Button br;
 
@@ -18,6 +19,8 @@ public class Aktywnosc_2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pierwsza);
 
+
+        nie = (TextView) findViewById(R.id.h) ;
         tak = (EditText) findViewById(R.id.edit);
         br = (Button) findViewById(R.id.gu);
 
@@ -25,14 +28,24 @@ public class Aktywnosc_2 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String napis = tak.getText().toString();
+                nie.setText(napis);
                 Intent intent = new Intent(getApplicationContext(),aktywnosc3.class);
                 intent.putExtra("cos", napis);
                 startActivity(intent);
 
             }
         });
+        if(savedInstanceState !=null){
+            String napis = savedInstanceState.getString("sws");
+            nie.setText(napis);
+        }
+    }
 
-
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String napis = nie.getText().toString();
+        outState.putString("sws",napis);
 
     }
 }
